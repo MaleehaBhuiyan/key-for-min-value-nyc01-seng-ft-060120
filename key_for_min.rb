6b82979bf7e5194eef1aff0d25f477351dfb9910
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-20 14:42:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 1000000
+  name_hash.each do |name, age|
+    if min_value < age
+      return name
+    end
+  end 
 end
