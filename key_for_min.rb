d7fe0cdd7343b87a6e0c0d89b45c20ec8273a14e
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-20 14:49:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 1000000
+  min_key = nil
+  name_hash.each do |name, age|
+    if age < min_value
+      min_value = age
+      min_key = name
+    end
+  end
+  min_key
 end
