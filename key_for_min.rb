66f16db134289aa27b9dd7c8014bd91885316b20
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-20 14:49:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_value = 1000000
+  min_key = nil
   name_hash.each do |name, age|
-    if min_value < age
+    if age < min_value
       min_value = age
+      min_key = name
     end
   end
+  min_key
 end
